58e0daef4cd6a2bb77d7773e84240512aa619d39
Susan Monnelly <smonnelly@newsbank.com>
2022-03-22 13:03:14 -0400

    OSD with svelte

---
+++ src/library/OSD.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { OpenSeaDragon } from "openseadragon";
+
+    OpenSeaDragon({
+        id: 'openSeaDragon',
+        prefixUrl: '/openseadragon/images/',
+        animationTime: 0.5,
+        blendTime: 0.1,
+        constrainDuringPan: true,
+        maxZoomPixelRatio: 2,
+        minZoomLevel: 1,
+        visibilityRatio: 1,
+        zoomPerScroll: 2,
+        tileSources:
+          '/openseadragon/beck.dzi',
+      })
+</script>
